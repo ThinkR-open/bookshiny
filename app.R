5a41e21d10435be697e4392e8728e88a16f29774
colin <colin@thinkr.fr>
2023-02-14 21:50:27 +0100

    feat: with envvar

--- app.R
+++ app.R
@@ -2,6 +2,8 @@
 # To deploy, run: rsconnect::deployApp()
 # Or use the blue button on top of this file
 
-pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
-options( "golem.app.prod" = FALSE)
-bookshiny::run_app_database() # add parameters here (if any)
+pkgload::load_all(export_all = FALSE, helpers = FALSE, attach_testthat = FALSE)
+options("golem.app.prod" = FALSE)
+get(
+  Sys.getenv("RUN_APP_FUNCTION", "run_app_css_graysacle")
+)()
