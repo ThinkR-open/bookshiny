d60d5bc053964689d1393b7b89b8bd524f9ad152
colin <colin@thinkr.fr>
2023-02-14 22:01:11 +0100

    feat: more explicit namespacing

--- R/mod_asyn_shiny_with.R
+++ R/mod_asyn_shiny_with.R
@@ -4,12 +4,12 @@
 #'
 #' @param id,input,output,session Internal parameters for {shiny}.
 #'
-#' @noRd 
+#' @noRd
 #'
-#' @importFrom shiny NS tagList 
-#' @importFrom future future plan multisession 
+#' @importFrom shiny NS tagList
+#' @importFrom future future plan multisession
 #' @import promises
-mod_asyn_shiny_with_ui <- function(id){
+mod_asyn_shiny_with_ui <- function(id) {
   ns <- NS(id)
   tagList(
     golem::activate_js(),
@@ -20,85 +20,90 @@ mod_asyn_shiny_with_ui <- function(id){
     br(),
     fluidPage(
       fluidRow(
-        column(6,
-               card_div(
-                 p("When you click on the button, it takes 5 second to generate this graphic."),
-                 actionButton(ns("generate"), "Generate !"),
-                 plotOutput(ns("graph"))
-               )
+        column(
+          6,
+          card_div(
+            p("When you click on the button, it takes 5 second to generate this graphic."),
+            actionButton(ns("generate"), "Generate !"),
+            plotOutput(ns("graph"))
+          )
         ),
-        column(6,
-               card_div(
-                 sliderInput(ns("lignes"),
-                             label = "Select random number of lines",
-                             min = 5,
-                             max = 10,
-                             value = 5),
-                 tableOutput(ns("table"))
-               )
+        column(
+          6,
+          card_div(
+            sliderInput(
+              ns("lignes"),
+              label = "Select random number of lines",
+              min = 5,
+              max = 10,
+              value = 5
+            ),
+            tableOutput(ns("table"))
+          )
         )
       ),
       br(),
       fluidRow(
         a_bttn(
           "See what's happen without {promises} and {future}",
-          href = "http://connect.thinkr.fr/without_p_f")
-        
+          href = "http://connect.thinkr.fr/without_p_f"
+        )
       )
-    )#,
+    ) # ,
     # shiny_busy_not_blocking(id = ns("async_shiny"))
-    
   )
 }
 
 #' asyn_shiny_with Server Function
 #'
-#' @noRd 
-mod_asyn_shiny_with_server <- function(input, output, session){
+#' @noRd
+mod_asyn_shiny_with_server <- function(input, output, session) {
   ns <- session$ns
-  
-  plan(multisession)
-  
+
+  future::plan(future::multisession)
+
   local <- reactiveValues()
-  
-  observeEvent( input$generate , {
-    ## Simulate long process
-    
-    future({
-      
-      Sys.sleep(5)
-      shinipsum::random_ggplot()
-    }) %...>%
-      (function(e){
-        
-        local$plot <- e 
-      }) %...!%
-      (function(e){
-        warning(e)
-      })
-  
-  showNotification(
-    p("The graphic is calculated but you can still use the application"),
-    closeButton = FALSE)
-    
-  message("Done !")
-  
-}, ignoreInit = TRUE)
 
-observeEvent( input$lignes , {
-  local$table <-  iris %>%
-    sample_n(input$lignes)
-})
+  observeEvent(
+    input$generate,
+    {
+      ## Simulate long process
+
+      future({
+        Sys.sleep(5)
+        shinipsum::random_ggplot()
+      }) %...>%
+        (function(e) {
+          local$plot <- e
+        }) %...!%
+        (function(e) {
+          warning(e)
+        })
+
+      showNotification(
+        p("The graphic is calculated but you can still use the application"),
+        closeButton = FALSE
+      )
+
+      message("Done !")
+    },
+    ignoreInit = TRUE
+  )
+
+  observeEvent(input$lignes, {
+    local$table <- iris %>%
+      sample_n(input$lignes)
+  })
 
 
-output$graph <- renderPlot({
-  req(local$plot)
-  local$plot
-})
+  output$graph <- renderPlot({
+    req(local$plot)
+    local$plot
+  })
 
-output$table <- renderTable({
-  local$table
-})
+  output$table <- renderTable({
+    local$table
+  })
 }
 
 ## To be copied in the UI
@@ -106,4 +111,3 @@ output$table <- renderTable({
 
 ## To be copied in the server
 # callModule(mod_asyn_shiny_with_server, "asyn_shiny_with_ui_1")
-
--- app.R
+++ app.R
@@ -2,7 +2,7 @@
 # To deploy, run: rsconnect::deployApp()
 # Or use the blue button on top of this file
 library(golem)
-pkgload::load_all(export_all = FALSE, helpers = FALSE, attach_testthat = FALSE)
+pkgload::load_all()
 options("golem.app.prod" = FALSE)
 get(
   Sys.getenv("RUN_APP_FUNCTION", "run_app_css_graysacle")
